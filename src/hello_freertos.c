3cdd437aabdb0526ddd97d381087d6f420a18c09
Jack <u1442284@utah.edu>
2026-09-16 12:57:42 -0600

    changed global params to passed ones

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -13,16 +13,18 @@
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
 
-int count = 0;
-bool on = false;
+
 
 #define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
 #define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
-void blink_task(__unused void *params) {
+void blink_task(void *params) {
     hard_assert(cyw43_arch_init() == PICO_OK);
+
+    int count = *(int *)params[0];
+    bool on = *(bool *)params[1];  
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
         if (count++ % 11) on = !on; // flip 10 outta 11 times
@@ -30,9 +32,9 @@ void blink_task(__unused void *params) {
     }
 }
 
-void main_task(__unused void *params) {
+void main_task(void *params) {
     xTaskCreate(blink_task, "BlinkThread",
-                BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
+                BLINK_TASK_STACK_SIZE, &params, BLINK_TASK_PRIORITY, NULL);
     char c;
     while(c = getchar()) {
         if (c <= 'z' && c >= 'a') putchar(c - 32);
@@ -41,14 +43,20 @@ void main_task(__unused void *params) {
     }
 }
 
+
 int main( void )
 {
     stdio_init_all();
+
+    static int count = 0;
+    static bool on = false;
+    void *params[2] = {&count, &on};
+
     const char *rtos_name;
     rtos_name = "FreeRTOS";
     TaskHandle_t task;
     xTaskCreate(main_task, "MainThread",
-                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
+                MAIN_TASK_STACK_SIZE, &params, MAIN_TASK_PRIORITY, &task);
     vTaskStartScheduler();
     return 0;
 }
