af8b62b7aad7aa9e4dfe259236f95feff5ca39e6
Matthew Crump <U0894108@umail.utah.edu>
2026-09-16 21:00:05 -0600

    Moved from static vars to declaring them before the forever loop

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -2,6 +2,7 @@
  * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
  *
  * SPDX-License-Identifier: BSD-3-Clause
+ * Edited by Matthew Crump and Will Phillips
  */
 
 #include <stdio.h>
@@ -20,23 +21,30 @@
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
+void toggle(int* count, bool* on) {
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, *on);
+    bool retval = *count % 11;
+    if (retval) *on = !(*on); // flip 10 outta 11 times
+    *count++;
+}
+
 void blink_task(void *params) {
     hard_assert(cyw43_arch_init() == PICO_OK);
 
-    int count = *(int *)params[0];
-    bool on = *(bool *)params[1];  
+    //int count = *(int *)params[0];
+    //bool on = *(bool *)params[1];  
+		int count = 0;
+		bool on = false;
     while (true) {
-        toggle(&count);
+        toggle(&count, &on);
         vTaskDelay(500); // 500 ms
     }
 }
 
-bool toggle(int* count, bool* on) {
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, *on);
-    bool retval = *count % 11;
-    if (retval) *on = !(*on); // flip 10 outta 11 times
-    count++;
-    return retval;
+char convert(char c) {
+    if (c <= 'z' && c >= 'a') return c - 32;
+    else if (c >= 'A' && c <= 'Z') return c + 32;
+    else return c;
 }
 
 void main_task(void *params) {
@@ -48,26 +56,21 @@ void main_task(void *params) {
     }
 }
 
-char convert(char c) {
-    if (c <= 'z' && c >= 'a') return c - 32;
-    else if (c >= 'A' && c <= 'Z') return c + 32;
-    else return c;
-}
 
 
 int main( void )
 {
     stdio_init_all();
 
-    static int count = 0;
-    static bool on = false;
-    void *params[2] = {&count, &on};
+//    static int count = 0;
+//    static bool on = false;
+//    void *params[2] = {&count, &on};
 
     const char *rtos_name;
     rtos_name = "FreeRTOS";
     TaskHandle_t task;
     xTaskCreate(main_task, "MainThread",
-                MAIN_TASK_STACK_SIZE, &params, MAIN_TASK_PRIORITY, &task);
+                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
     vTaskStartScheduler();
     return 0;
 }
